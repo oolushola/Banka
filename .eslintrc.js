afa3d86aaa2a3bc8f8fa1a52b56be870d250a5b5
oolushola <odejobiolushola@gmail.com>
2019-04-05 14:25:35 +0100

    chore(linting-setup):setup linting packages [Delivers #165145385]

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    //"extends": "eslint:recommended",
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi":["error", "always"],
+        "quotes": ["error", "single"],
+        "console":["true"]
+    }
+};
