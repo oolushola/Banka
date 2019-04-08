5f4d25d83c36da15d74a0b52741913d4dd1f6473
oolushola <odejobiolushola@gmail.com>
2019-04-08 12:28:58 +0100

    feature(user-resetpassword):build endpoint to reset password [Delivers #165169596]

--- api/routes/bankaroutes.js
+++ api/routes/bankaroutes.js
@@ -8,5 +8,7 @@ router.get('/api/v1/register', userController.getSignUp);
 router.post('/api/v1/auth/register', userController.userSignUp);
 router.get('/api/v1/auth/user-login', userController.getUserLogin);
 router.post('/api/v1/auth/user-login', userController.userLogin);
+router.get('/api/v1/user/password-reset', userController.getResetPassword);
+router.post('/api/v1/user/password-reset', userController.resetPassword);
 router.post('/api/v1/user/create-bank-account/:id', userController.createBankAccount);
 export default router;
