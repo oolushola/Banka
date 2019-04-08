7fd6350069c7d4c79f9cc4888129b8f01fff2bad
oolushola <odejobiolushola@gmail.com>
2019-04-08 11:01:45 +0100

    feature(userlogin-apiendpoint):build user login endpoint [Delivers #165169521]

--- api/controllers/users-controller.js
+++ api/controllers/users-controller.js
@@ -46,6 +46,29 @@ class userController {
     const token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 });
     return res.status(201).send({ auth: true, user, token });
   }
+
+  static getUserLogin(req, res, next) {
+    fs.readFile(`${filePath}/user-sign-in.html`, (err, contents) => {
+      if (err) {
+        return next(err);
+      }
+      res.writeHead(200, { 'content-type': 'text/html' });
+      return res.end(contents, 'utf8');
+    });
+  }
+
+  static userLogin(req, res) {
+    // validate for user email;
+    // validate for password request;
+    const userFound = users.find(userdb => userdb.email === req.body.email);
+    if (!userFound) return res.status(404).send({ status: 'failed', msg: 'user not found.' });
+
+    const passwordIsValid = bcrypt.compareSync(req.body.password, userFound.password);
+    if (!passwordIsValid) return res.status(401).send({ auth: 'false', token: null });
+
+    const token = jwt.sign({ id: userFound.id }, config.secret, { expiresIn: 86400 });
+    return res.status(200).send({ auth: true, token, userFound });
+  }
 }
 
 
--- api/routes/bankaroutes.js
+++ api/routes/bankaroutes.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/', userController.index);
 router.get('/api/v1/register', userController.getSignUp);
 router.post('/api/v1/auth/register', userController.userSignUp);
+router.get('/api/v1/auth/user-login', userController.getUserLogin);
+router.post('/api/v1/auth/user-login', userController.userLogin);
 
 export default router;
