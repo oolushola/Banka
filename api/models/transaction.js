3d167ca28a4e4cd68ebaaf0f5177a2ff71ba3805
oolushola <odejobiolushola@gmail.com>
2019-04-11 18:02:40 +0100

    feature(staff-login):staff login endpoint [Delivers #165289727]

---
+++ api/models/transaction.js
@@ -0,0 +1,14 @@
+const transactions = [
+  {
+    id: 1,
+    createdOn: new Date(),
+    type: 'credit',
+    accountNumber: 1234567810,
+    cashier: 4,
+    amount: 1000,
+    oldBalance: 1000,
+    newBalance: 1000,
+  },
+];
+
+export default transactions;
--- api/routes/bankaroutes.js
+++ api/routes/bankaroutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import userController from '../controllers/users-controller';
+import staffController from '../controllers/staff-controller';
 
 const router = Router();
 
@@ -8,10 +9,15 @@ router.get('/api/v1/register', userController.getSignUp);
 router.post('/api/v1/auth/register', userController.userSignUp);
 router.get('/api/v1/auth/user-login', userController.getUserLogin);
 router.post('/api/v1/auth/user-login', userController.userLogin);
+router.put('/api/v1/update-profile/:id', userController.updateProfile);
 router.get('/api/v1/user/password-reset', userController.getResetPassword);
 router.post('/api/v1/user/password-reset', userController.resetPassword);
 router.post('/api/v1/user/create-bank-account/:id', userController.createBankAccount);
 router.get('/api/v1/user', userController.specificUser);
 router.post('/api/v1/user/change-password/:id', userController.changePassword);
-router.get('/api/v1/users-list', userController.getAllUsers);
+
+// Route defined for the staffs
+router.get('/api/v1/staff/login', staffController.getStaffLogin);
+
+
 export default router;
