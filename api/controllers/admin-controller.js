13a99c7f150575042e769540a3c1c35c35096ffb
oolushola <odejobiolushola@gmail.com>
2019-04-24 20:08:04 +0100

    feature(admin-login):admin login through persisteddata [DELIVERS #165300660]

--- api/controllers/admin-controller.js
+++ api/controllers/admin-controller.js
@@ -13,8 +13,26 @@ const pool = new Pool({
 
 
 class adminController {
+  static adminLogin(req, res, next) {
+    const { email, password } = req.body;
 
-  
+    const checkLoginInfo = 'SELECT id, email, password, user_type, is_admin FROM users WHERE email = $1 ';
+    pool.query(checkLoginInfo, [email], (err, result) => {
+      if (err) return next(err);
+      if (result.rows.length <= 0) return res.status(404).send({ auth: false, token: null, msg: 'Invalid login details ' });
+      if ((result.rows[0].user_type !== 'admin') && (result.rows[0].is_admin !== true)) return res.status(401).send({ status: 401, auth: false, msg: 'unauthorized' });
+
+      const getPassword = bcrypt.compareSync(password, result.rows[0].password);
+      if (!getPassword) {
+        return res.status(401).send({
+          status: 401, auth: false, token: null, msg: 'invalid login details',
+        });
+      }
+
+      const token = jwt.sign({ id: result.rows[0].id }, config.secret, { expiresIn: 86400 });
+      return res.status(200).send({ status: 200, auth: true, token });
+    });
+  }
 }
 
 export default adminController;
