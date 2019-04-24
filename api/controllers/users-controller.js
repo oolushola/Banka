766723fbd03822e084c1d27c38f66db5f192b58a
oolushola <odejobiolushola@gmail.com>
2019-04-24 14:08:53 +0100

    feature(signup-user-db):user can sign up [DELIVERS #165169509]

--- api/controllers/users-controller.js
+++ api/controllers/users-controller.js
@@ -16,6 +16,23 @@ class userController {
     res.send('Welcome to banka');
   }
 
+  static userSignUp(req, res) {
+    const { email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    const checkQueryText = 'SELECT email FROM users where email = $1';
+    pool.query(checkQueryText, [email], (err, result) => {
+      if (err) throw err;
+      if (result.rows.length >= 1) {
+        return res.status(409).send({ status: 409, msg: 'record exists' });
+      }
+      const insertQuery = 'INSERT INTO users (email, password, user_type, is_admin) VALUES ($1, $2, $3, $4)';
+      pool.query(insertQuery, [email, hashedPassword, 'client', false], (error) => {
+        if (error) throw error;
+        const token = jwt.sign({ email }, config.secret, { expiresIn: 86400 });
+        return res.status(201).send({ status: 201, auth: true, token });
+      });
+    });
+  }
 }
 
 
